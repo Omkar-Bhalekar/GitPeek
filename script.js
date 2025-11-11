9427801f791c96675acfd1d1b8c5fe917b7982e3
Omkar Bhakekar <omkarbhalekar2003@gmail.com>
2025-11-11 15:47:27 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelector('#button').addEventListener('click', function () {
+
+    let username = document.querySelector('#myusername').value;
+    let requestURL = `https://api.github.com/users/${username}`;
+    console.log("username = " + username)
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', requestURL);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            const data = JSON.parse(xhr.responseText)
+            
+            document.querySelector('#container').innerHTML =
+            `<div id="card">
+                <div id="image"></div>
+                <div id="followers"></div>
+            </div>`
+
+        document.querySelector('#image').innerHTML = `<img src="${data.avatar_url}"></img>`
+
+        document.querySelector('#followers').innerHTML = `<p>${data.name}</p><br><p>Total Followers: ${data.followers}</p><br><p>Total Repo Count: ${data.public_repos}</p>`
+        }
+        
+        else{
+            document.querySelector('#container').innerHTML = `<p>User Does Not Exist!!</p>`
+        }
+    }
+
+    xhr.send();
+})
+
